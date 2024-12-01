85e9887ffeb8e93f75a3da7eb61408819556a036
Luciferstrike123 <130897548+Luciferstrike123@users.noreply.github.com>
2024-12-01 10:29:43 +0700

    Update Profile page

--- views/src/routes/route.jsx
+++ views/src/routes/route.jsx
@@ -3,16 +3,16 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import Login from "../components/login/Login";
 import Home from "../components/home/Home";
 import Login1 from "../components/login1/Login1";
-// import AdminLogin1 from "../components/login1/AdminLogin1";
+import AdminLogin1 from "../components/login1/AdminLogin1";
 import Choose from "../components/choose/Choose";
 import Completeprint from "../components/announcement/completeprint";
 import Upload from "../components/upload/upload";
 // import Adminusers from "../components/profile/adminusers";
-// import Users from "../components/profile/users";
+import Users from "../components/profile/users";
 import Printlocate from "../components/printlocate/Printlocate";
 // import Help from "../components/help/Help";
 // import Payment from "../components/payment/payment";
-// import AdminHome from "../components/home/AdminHome";
+import AdminHome from "../components/home/AdminHome";
 // import Cantbuy from "../components/announcement/cantbuy";
 // import Successbuy from "../components/announcement/successbuy";
 const Routing = () => {
@@ -27,8 +27,8 @@ return (
         <Route path="/Profile" element={<Users />} />
         <Route path="/PrintLocate" element={<Printlocate />} />
         {/* <Route path="/Payment" element={<Payment />} /> */}
-        {/* <Route path="/AdminLogin1" element={<AdminLogin1 />} /> */}
-        {/* <Route path="/AdminHome" element={<AdminHome />} /> */}
+        <Route path="/AdminLogin1" element={<AdminLogin1 />} />
+        <Route path="/AdminHome" element={<AdminHome />} />
         {/* <Route path="/AdminUsers" element={<Adminusers />} /> */}
         {/* <Route path="/help" element={<Help />} /> */}
         {/* <Route path="/successbuy" element={<Successbuy />} /> */}
